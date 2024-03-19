9c87fb32ec6a868d4c861c5a7d0063ec373a9a11
kariastrand <karinastrand@yahoo.se>
2024-03-19 12:59:51 +0100

    added files for serach books

---
+++ src/components/searchbooks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'searchbooks.css';
+import showbook from './showbook';
+import bookcard from '/bookcard';
+import books from './books.json';
+
+
+export default function searchbooks() {
+  return (
+    <div>searchbooks</div>
+  )
+}
