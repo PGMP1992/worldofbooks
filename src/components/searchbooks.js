163eb0bb404013cb463b810e3b767a39882e1ca3
kariastrand <karinastrand@yahoo.se>
2024-03-19 14:00:34 +0100

    corrected spelling of images and imports in searchbooks

--- src/components/searchbooks.js
+++ src/components/searchbooks.js
@@ -4,25 +4,26 @@ import showbook from './showbook.js';
 import bookcard from './bookcard.js';
 import books from './books.json';
 import Img1 from '../images/100gomistakesandhowtoavoidthem.png';
-import Img2 from '../images/huckleberryfin.jifi';
-import Img3 from '../images/huckleberryfin.jifi';
-import Img4 from '../images/cleancoder.png';
+import Img2 from '../images/huckleberryfin.jfif';
+import Img3 from '../images/huckleberryfin.jfif';
+import Img4 from '../images/thecleancoder.png';
 import Img5 from '../images/learningvue.png';
 import Img6 from '../images/imnotsleepy.png';
 import Img7 from '../images/japantravelguide.png';
-import Img8 from '../images/nepltravelguide.png';
+import Img8 from '../images/grymms.jfif';
 import Img9 from '../images/pocketmadeira.png';
 import Img10 from '../images/whatmakesagarden.png';
-import Img11 from '../images/100gomistakesandhowtoavoidthem.png';
-import Img12 from '../images/100gomistakesandhowtoavoidthem.png';
-import Img13 from '../images/100gomistakesandhowtoavoidthem.png';
-import Img14 from '../images/100gomistakesandhowtoavoidthem.png';
-import Img15 from '../images/100gomistakesandhowtoavoidthem.png';
-import Img16 from '../images/100gomistakesandhowtoavoidthem.png';
-import Img17 from '../images/100gomistakesandhowtoavoidthem.png';
-import Img18 from '../images/100gomistakesandhowtoavoidthem.png';
-import Img19 from '../images/100gomistakesandhowtoavoidthem.png';
-import Img20 from '../images/100gomistakesandhowtoavoidthem.png';
+import Img11 from '../images/100herbstogrow.png'
+import Img12 from '../images/beautyandthebeast.jfif';
+import Img13 from '../images/grymms.jfif';
+import Img14 from '../images/olivertwist.jfif';
+import Img15 from '../images/alice.jfif';
+import Img16 from '../images/pinocchio.jfif';
+import Img17 from '../images/beautyandthebeast.jfif';
+import Img18 from '../images/grymms.jfif';
+import Img19 from '../images/olivertwist.jfif';
+import Img20 from '../images/alice.jfif';
+import Img21 from '../images/pinocchio.jfif';
 
 
 
