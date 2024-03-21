991edaccb006c377548d4f4bb1e30c03fe116cb7
kariastrand <karinastrand@yahoo.se>
2024-03-21 12:09:02 +0100

    copy of wob

--- src/components/searchbooks.js
+++ src/components/searchbooks.js
@@ -36,24 +36,6 @@ export default function Searchbooks() {
 
 
   
-
-  return(
-    
-    <div className='book' key={values.id}>
-    <Bookcard
-    id={values.id}
-    title={values.title}
-    author={values.author}
-    type={values.type}
-    category={values.category}
-    price={values.price}
-    src={values.src}
-    />
- 
-  </div>
-  );
-    }
-}
   return (
     
    <div>
